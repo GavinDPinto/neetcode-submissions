ec4b8776f989ae377d9df4929f0070a34d4a39ee
Gavin Pinto <gavinpinto620@gmail.com>
2026-08-08 00:27:04 -0400

    Add: three-integer-sum - submission-16

---
+++ Data Structures & Algorithms/three-integer-sum/submission-16.py
@@ -0,0 +1,46 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        # Sort nums
+        self.heapsort(nums)
+        print(nums)
+        ret_set = set()
+        # Solve twoSum where target = nums[i] and numbers = nums[i+1:] for each i and add the triplet of indices to ret
+        for i in range(len(nums) - 2):
+            j, k = i + 1, len(nums) - 1
+            while j < k:
+                if nums[j] + nums[k] == -nums[i]:
+                    ret_set.add((nums[i], nums[j], nums[k]))
+                    j += 1
+                    k -= 1
+                elif nums[j] + nums[k] < -nums[i]:
+                    j += 1
+                else:
+                    k -= 1
+        return [list(x) for x in ret_set]
+    
+    def heapsort(self, nums):
+        # Max-heapify (will make it so it sorts in ascending order)
+        for i in range(len(nums)):
+            cur_idx = i
+            while cur_idx > 0 and nums[cur_idx] > nums[(cur_idx - 1) // 2]:
+                nums[cur_idx], nums[(cur_idx - 1) // 2] = nums[(cur_idx - 1) // 2], nums[cur_idx]
+                cur_idx = (cur_idx - 1) // 2
+        
+        # Sort by repeated popping
+        for i in range(len(nums) - 1, 0, -1):
+            nums[i], nums[0] = nums[0], nums[i]
+            cur_idx = 0
+            stop = 0
+            while stop == 0:
+                if (cur_idx * 2 + 2 < i and nums[cur_idx * 2 + 2] > nums[cur_idx] and nums[cur_idx * 2 + 2] > nums[cur_idx * 2 + 1]):
+                    nums[cur_idx], nums[cur_idx * 2 + 2] = nums[cur_idx * 2 + 2], nums[cur_idx]
+                    cur_idx = cur_idx * 2 + 2
+                elif (cur_idx * 2 + 1 < i and nums[cur_idx * 2 + 1] > nums[cur_idx]):
+                    nums[cur_idx], nums[cur_idx * 2 + 1] = nums[cur_idx * 2 + 1], nums[cur_idx]
+                    cur_idx = cur_idx * 2 + 1
+                else:
+                    stop = 1
+        
+
+
+            
