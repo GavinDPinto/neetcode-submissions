442bcde57833ccabef9751d86872a86bf8a89028
Gavin Pinto <gavinpinto620@gmail.com>
2026-08-10 19:44:44 -0400

    Add: longest-repeating-substring-with-replacement - submission-1

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        if len(s) < 2: return len(s)
+        
+        maxf = 0 # all-time max frequency of a letter in any valid window
+        freqs = [0] * 26
+        left, right = 0, 0
+
+        c = s[right]
+        freqs[ord(c)-ord('A')] += 1
+        cur_freq = freqs[ord(c)-ord('A')]
+        maxf = max(cur_freq, maxf)
+
+        while right < len(s) - 1:
+            if 1+right-left - maxf > k: # current window is invalid
+                freqs[ord(s[left])-ord('A')] -= 1
+                left += 1
+            else: # window valid; expand to the right and update freqs and maxf if needed
+                right += 1 
+                c = s[right]
+                freqs[ord(c)-ord('A')] += 1
+                cur_freq = freqs[ord(c)-ord('A')]
+                maxf = max(cur_freq, maxf)
+        
+        return min(len(s), maxf+k)
