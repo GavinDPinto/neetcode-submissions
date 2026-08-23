16e4da9d95417b7011b599bed71ca6a2f2ee5208
Gavin Pinto <gavinpinto620@gmail.com>
2026-08-23 19:52:04 -0400

    Add: car-fleet - submission-9

---
+++ Data Structures & Algorithms/car-fleet/submission-9.py
@@ -0,0 +1,12 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pairs = sorted(zip(position, speed), reverse=True)
+        fleets = 0
+        max_time = 0.0
+        for pos, speed in pairs:
+            time = (target - pos) / speed
+            if not fleets or time > max_time:
+                fleets += 1
+                max_time = time
+        
+        return fleets
